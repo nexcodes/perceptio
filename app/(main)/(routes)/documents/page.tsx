785a31b65b0cc363a5a6a7e986ea7e9d8d0d2c83
nexcodes <mohidnaeem41@gmail.com>
2023-11-07 23:16:11 +0500

    updated Sidebar / Created Convex Schema for documents

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,8 +1,36 @@
-const DocumentsPage = () => {
-  return (
-    <div>
-    </div>
-  )
-}
+"use client";
 
-export default DocumentsPage
+import Image from "next/image";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+
+const DocumentsPage = () => {
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        width={300}
+        height={300}
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        width={300}
+        height={300}
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2>{`Welcome to ${user?.firstName}'s Perceptio`}</h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
+};
+
+export default DocumentsPage;
